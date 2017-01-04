9dfab9c32fd7f23b8f78c3d8c2b492aad81c3d44
Ganessh Kumar <rpganesshkumar@gmail.com>
2017-01-04 06:00:21 +0530

    AuthForm streamlined

---
+++ client/components/auth/SignupForm.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import { Field, reduxForm } from 'redux-form'
+import { Meteor } from 'meteor/meteor';
+
+import { loginFormSelected } from '../../actions/authActions';
+import {
+  validate, warn, renderName, renderPassword
+} from '../../helpers/authHelpers';
+
+const SignupForm = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <div className="auth-form-container pt-control-group pt-vertical">
+      <form onSubmit={handleSubmit}>
+        <div className="pt-control-group pt-vertical">
+          <Field name="username" type="text" component={renderName} label="Username"/>
+          <Field name="password" type="password" component={renderPassword} label="Password"/>
+          <button type="submit"className="pt-button pt-large pt-intent-primary">Signup</button>
+        </div>
+      </form>
+
+      <div className="auth-form-link-container pt-ui-text-large">
+        Already have an account? &nbsp;
+        <span className="auth-form-link"
+              onClick={props.loginLinkClicked}>
+            Login
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default reduxForm({
+  form: 'auth',  // a unique identifier for this form
+  validate,                // <--- validation function given to redux-form
+  warn                     // <--- warning function given to redux-form
+})(SignupForm)
