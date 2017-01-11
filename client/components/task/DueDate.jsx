4b2cd3644d78610904cbc02d2a9702896d18b555
Ganessh Kumar <rpganesshkumar@gmail.com>
2017-01-12 02:00:33 +0530

    making all completed tasks duedate in same style

--- client/components/task/DueDate.jsx
+++ client/components/task/DueDate.jsx
@@ -4,21 +4,29 @@ import classnames from 'classnames';
 
 export default DueDate = (props) => {
   if (props.task.dueDate) {
-
     let dueDate = new Date(props.task.dueDate);
     let difference = moment(dueDate).diff(moment(new Date()), 'days');
-    let dueDateClass = classnames({
-      'over-due': difference < 0,
-      'today': difference == 0,
-      'this-week': difference > 0 && difference < 6,
-      'someday': difference >= 6
-    });
 
-    return (
-      <div className='due-date'>
-        <span className={dueDateClass}> {moment(dueDate).fromNow()} </span>
-      </div>
-    );
+    if (props.task.checked) {
+      return (
+        <div className='due-date'>
+          <span className='done'> {moment(dueDate).fromNow()} </span>
+        </div>
+      );
+    } else {
+      let dueDateClass = classnames({
+        'over-due': difference < 0,
+        'today': difference == 0,
+        'this-week': difference > 0 && difference < 6,
+        'someday': difference >= 6
+      });
+
+      return (
+        <div className='due-date'>
+          <span className={dueDateClass}> {moment(dueDate).fromNow()} </span>
+        </div>
+      );
+    }
 
   } else {
     return (
